7ebbae0618c81639bf3fa666b53eec360c0ef186
Samantha Arellano <Sambollano54@gmail.com>
2021-07-21 20:48:22 -0500

    update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const { generate } = require("rxjs")
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license = "None") {
+    return `${license}`
+  }
+  else {"None"}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseBadge(license) {
+  if (license = "None") {
+    return `${license}`
+  }
+  else {"None"}
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(No_license) {
+  return `${renderLicenseBadge(No.license)}
+  ${renderLicenseLink(No.license_link)}     
+` 
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# Title
+   ${data.title}
+
+   # Contents
+   - money
+   - skill
+   - job
+   - test
+   - age
+   - install
+   - contact
+   - name
+   - title
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports - generateMarkdown;
